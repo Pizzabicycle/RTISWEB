ae7664cbf6c25d4a67b8fa72a194053581a486ca
chan <dzm2111@naver.com>
2024-10-24 22:57:10 +0900

    [CHANGED] 프로젝트 생성

---
+++ src/main/java/com/rtisweb/RtiswebApplication.java
@@ -0,0 +1,13 @@
+package com.rtisweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtiswebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtiswebApplication.class, args);
+    }
+
+}
